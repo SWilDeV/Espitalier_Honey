f201aefb2b979a8df78a7c7659bd36649c489284
sean wilson <sean0669@gmail.com>
2020-07-25 13:43:06 -0400

    carousel, btns

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,25 +1,43 @@
 //display navbar at second section
 $(document).ready(function(){       
     var scroll_start = 0;
-    var startchange = $('#second-img');
+    var startchange = $('#second-sec');
     var offset = startchange.offset();
-     if (startchange.length){
-    $(document).scroll(function() { 
-       scroll_start = $(this).scrollTop();
-       if(scroll_start > offset.top) {
-           $(".navbar").css('display', 'flex');
-        } else {
-           $('.navbar').css('display', 'none');
-        }
-    });
-     }
- });
+    if (startchange.length){
+        $(document).scroll(function() { 
+        scroll_start = $(this).scrollTop();
+            if(scroll_start > offset.top) {
+                $(".navbar").css('display', 'flex');
+            } 
+            else {
+                $('.navbar').css('display', 'none');
+            }
+        });
+    }
+});
 
- function scrollToAnchor(aid){
+
+function scrollToAnchor(aid){
     var aTag = $(aid);
     $('html,body').animate({scrollTop: aTag.offset().top+1},'slow');
 }
 
-$("#bienvenue").click(function() {
-    scrollToAnchor('#second-img');
- });
+$("#welcome-text").click(function() {
+    scrollToAnchor('#second-sec');
+});
+
+$("#accueil-btn").click(function() {
+    scrollToAnchor('#welcome-section');
+});
+
+$("#decouvrez-btn").click(function() {
+    scrollToAnchor('#second-sec');
+});
+
+$("#produits-btn").click(function() {
+    scrollToAnchor('#third-sec');
+});
+
+$("#contact-btn").click(function() {
+scrollToAnchor('#contact-section');
+});
